5c17a3bd6bab7ea68ad93ce1215e99499f9d723a
tmralmeida <tmr.almeida96@gmail.com>
2022-04-18 16:15:24 +0200

    simulator ready

--- README.md
+++ README.md
@@ -11,10 +11,77 @@ conda create --name <env_name> --file requirements.yml
 
 ## Current status
 
-* [ ] Simulator
-  * [ ] agent in the environment
-  * [ ] dirty in the environment 
+* [x] Simulator
+  * [x] agent in the environment
+  * [x] obstacles in the environment
+  * [x] dirty in the environment 
 * [ ] Smart agent 
 
-## Usage 
+## Instructions
 
+### Arguments
+
+There is only one argument (`--num_cells`):
+
+```
+
+python run.py --help         
+usage: run.py [-h] [--num_cells NUM_CELLS]
+
+Vacuum cleaning smart agent
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --num_cells NUM_CELLS, -nc NUM_CELLS
+                        Grid world's side number of cells
+
+```
+
+The number of side cells can vary within the range `[5,11]`.
+
+So, if one wants to run the training in an environment with 7 grid cells:
+
+```
+python run.py --num_cells 7
+```
+
+### Usage
+
+After launching the simulator, the user should use the buttons of his mouse:
+* button left: it places/removes the robot in/from the environment
+* button right: it places/removes obstacles and dirty in/from the environment
+
+**Constraints/assumptions**:
+
+* there is only one robot
+* there is a maximum amount of obstacles =  `num_cells`
+* there is no limit for the amount of "dirty"
+* button right: the user should first place the obstacles and then the dirty
+* nevertheless, the user can remove any object by simply clicking again on the respective occupied cell with the respective button
+* reward of hitting against an obstacle: -10
+* reward of moving in any direction: -1
+* reward of vacuuming one cell with dirty: +1
+* the state keeps track of the rewards in each cell, where `0` means the robot's location
+
+The user can keep track on the changes on the environment by looking at the logs in the terminal. For instance, an 11-Grid world environment and the respective `state` should look like this: 
+
+```
+<p float="left">
+    <img src="docs/example_gs11.png" width="384" height="220"/>
+     [[  0.   1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.]
+      [ -1.  -1. -10. -10.  -1. -10. -10.  -1.  -1. -10.  -1.]
+      [  1.  -1.  -1.   1.  -1.  -1.  -1.  -1.   1.  -1. -10.]
+      [  1.  -1. -10.  -1. -10.  -1.   1.  -1.  -1.  -1.  -1.]
+      [ -1.  -1.  -1.  -1.  -1. -10.  -1.  -1.   1.  -1.  -1.]
+      [ -1.   1. -10.  -1.  -1.  -1.   1.  -1.  -1.   1.  -1.]
+      [ -1.  -1.  -1. -10.  -1.  -1.  -1.  -1.  -1.  -1.  -1.]
+      [ -1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.   1.]
+      [ -1.  -1.  -1.  -1.  -1.   1.  -1.   1.  -1.  -1.  -1.]
+      [ -1.  -1.  -1.   1.  -1.  -1.  -1.  -1.  -1.  -1.  -1.]
+      [ -1.  -1.  -1.  -1.  -1.  -1.  -1.   1.  -1.  -1.  -1.]]
+</p>
+```
+
+
+
+After designing the environment, click `p` to run the training of the smart agent.
--- simulator/grid_world.py
+++ simulator/grid_world.py
@@ -1,6 +1,11 @@
 import numpy as np
-import tkinter
 from tkinter import *
+
+
+ROBOT_VALUE = 0
+OBSTACLE_VALUE = -10
+DIRTY_VALUE = 1
+FREE_VALUE = -1
 
 
 class GridWorld():
@@ -8,38 +13,126 @@ class GridWorld():
                  nc : int) -> None:
         self.side = nc*100
         self.nc = nc # number of cells
-        self.no = int(self.nc/2)
+        self.no = self.nc # number max of obstacles
+        self.nr = 1 # number maximum of robots
         self.root = Tk()    
         self.root.minsize(self.side, self.side)     
         self.root.title("Grid World for vacuum cleaning")  
         self.root.resizable(0,0) # no maximize
         self.__init_vars()
         
+    
     def __init_vars(self):
         self.clicks_robot, self.clicks_obstacles, self.clicks_dirty = 0, 0, 0
-        self.state = np.zeros(self.nc)
-        
+        self.state = np.ones((self.nc, self.nc)) * FREE_VALUE 
+        print("\nInitial state:\n", self.state)
 
-    def coord_robot(self, event):
-        pix_x = event.x         # Get x and y co-ordinate of the click event
-        pix_y = event.y
-        coord_x = int(pix_x / 100)
-        coord_y = int(pix_y / 100)
-        print(f"Updating robot to x = {coord_x} and y = {coord_y}. Thank you for placing the robot!\n \
-                Now, please place {self.no} obstacles with the right button of your mouse.")
+
+    def __update_cell(self, x : int, y : int, type : str):
+        if type == "robot":
+            self.state[y, x] = ROBOT_VALUE if self.state[y, x] == FREE_VALUE else FREE_VALUE 
+        elif type == "obs": # obstacle
+            self.state[y, x] = OBSTACLE_VALUE if self.state[y, x] == FREE_VALUE else FREE_VALUE
+        elif type == "dirty":
+            self.state[y, x] = DIRTY_VALUE if self.state[y, x] == FREE_VALUE  else FREE_VALUE 
+
+            
+    def __occ_robot(self, x : int, y : int):
+        return self.state[y, x] == ROBOT_VALUE
+    
+
+    def __occ_obstacle(self, x : int, y : int):
+        return self.state[y, x] == OBSTACLE_VALUE
 
     
+    def __occ_dirty(self, x : int, y : int):
+        return self.state[y, x] == DIRTY_VALUE
+
+
+    def __free_cell(self, x : int, y : int):
+        return not self.state[y, x] == ROBOT_VALUE and not self.state[y, x] == OBSTACLE_VALUE and not self.state[y, x] == DIRTY_VALUE
+
+
+    def coord_robot(self, event):
+        pix_x = event.x         
+        pix_y = event.y
+        print(pix_x, pix_y)
+        coord_x = int(pix_x / 100)
+        coord_y = int(pix_y / 100)
+        x0_paint,y0_paint = coord_x * 100 + 3, coord_y * 100 + 3
+        x1_paint,y1_paint = x0_paint + 94, y0_paint + 94
+        if self.clicks_robot < self.nr and self.__free_cell(coord_x, coord_y):
+            self.clicks_robot += 1        
+            self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#000", fill="#000") # painting black
+            # updating cell in the state 
+            self.__update_cell(coord_x, coord_y, "robot")
+            print("Placing robot at", (coord_y, coord_x))
+            print("New state\n", self.state)
+            
+        else:
+            # if it is occupied by the robot -> remove the robot
+            if self.__occ_robot(coord_x, coord_y):
+                self.clicks_robot -= 1
+                self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#fff", fill="#fff") # re-painting white
+                print("Removing robot from", (coord_y, coord_x))
+                self.__update_cell(coord_x, coord_y, "robot")
+                print("New state\n", self.state)
+            else:
+                print("Not occupied by the robot!")
+           
+           
+               
     def coords_obs_dirt(self, event):
         pix_x = event.x         # Get x and y co-ordinate of the click event
         pix_y = event.y
         coord_x = int(pix_x / 100)
         coord_y = int(pix_y / 100)
-        print(f"Updating obstacles or dirty to x = {coord_x} and y = {coord_y}")
+        x0_paint,y0_paint = coord_x * 100 + 3, coord_y * 100 + 3
+        x1_paint,y1_paint = x0_paint + 94, y0_paint + 94
+        if self.clicks_obstacles < self.no and self.__free_cell(coord_x, coord_y):
+            self.clicks_obstacles += 1
+            self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#f50", fill="#f50") # painting black
+            # updating cell in the state 
+            self.__update_cell(coord_x, coord_y, "obs")
+            print(f"Placing obstacles at", (coord_y, coord_x))
+            print("New state\n", self.state)
+        
+        else:
+            if self.__occ_obstacle(coord_x, coord_y): # occupied by an obstacle
+                self.clicks_obstacles -= 1
+                self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#fff", fill="#fff") # re-painting white
+                print("Removing obstacle from", (coord_y, coord_x))
+                self.__update_cell(coord_x, coord_y, "obs")
+                print("New state\n", self.state)
+            
+            elif self.__occ_dirty(coord_x, coord_y): # occupied by dirty
+                self.clicks_dirty -= 1
+                self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#fff", fill="#fff") # re-painting white
+                print("Removing dirty from", (coord_y, coord_x))
+                self.__update_cell(coord_x, coord_y, "dirty")
+                print("New state\n", self.state)
+            
+            elif self.__occ_robot(coord_x, coord_y): # occupied by the robot
+                print("Not occupied by obstacles or dirty. To remove the robot please use left button of your mouse!")
+            
+            elif  self.__free_cell(coord_x, coord_y): # let's start placing dirty
+                self.clicks_dirty += 1
+                self.canvas.create_rectangle(x0_paint,y0_paint,x1_paint,y1_paint, outline="#00ff7f", fill="#00ff7f") # re-painting white
+                self.__update_cell(coord_x, coord_y, "dirty")
+                print(f"Placing dirty at", (coord_y, coord_x))
+                print("New state\n", self.state)
+            
+            else:
+                raise Exception("Designer needs to double check button right")
+                
+        
+            
+            
         
 
     def get_world(self):
-        canvas = Canvas(self.root,height = self.side,width = self.side,bd=0)
-        canvas.pack(anchor=CENTER)
+        self.canvas = Canvas(self.root,height = self.side,width = self.side,bd=0,bg="#fff")
+        self.canvas.pack(anchor=CENTER)
         
         # drawing walls
         # canvas.create_line(0,0,self.side, 0, self.side, self.side, 0, self.side, width=5)
@@ -48,13 +141,13 @@ class GridWorld():
         # drawing lines
         for x in range(self.nc + 1):
             # vertical line
-            canvas.create_line(100*x,0,100*x,self.side,width=5)
+            self.canvas.create_line(100*x,0,100*x,self.side,width=5)
             for y in range(self.nc + 1):
                 # horizontal line
-                canvas.create_line(0,100*y,self.side, 100*y,width=5)
+                self.canvas.create_line(0,100*y,self.side, 100*y,width=5)
             
-        canvas.bind("<Button-1>", self.coord_robot)
-        canvas.bind("<Button-3>", self.coords_obs_dirt)
+        self.canvas.bind("<Button-1>", self.coord_robot)
+        self.canvas.bind("<Button-3>", self.coords_obs_dirt)
         print("\nPlease place the robot with the left button of your mouse.")
 
         
